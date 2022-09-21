01c15d222396c7338a12f99efc65307ba5236c18
fleiscjl <113860663+fleiscjl@users.noreply.github.com>
2022-09-21 12:33:21 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        System.out.println();
+
+        int numToExamine = 12;
+        int Mudulo;
+
+        Mudulo = numToExamine / 2;
+        System.out.println("Your number" + " " + Mudulo + " is even");
     }
 }
